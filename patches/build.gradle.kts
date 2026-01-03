c4de2855a2c677d0b905fc082f8030f2bbc2e200
Eric Ahn <wchill1337@gmail.com>
2026-01-02 19:31:59 -0800

    chore: Update project files with correct name

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -2,12 +2,12 @@ group = "app.morphe"
 
 patches {
     about {
-        name = "Morphe Patches template"
-        description = "Patches template for Morphe"
-        source = "git@github.com:MorpheApp/morphe-patches-template.git"
-        author = "Morphe"
-        contact = "contact@morphe.software"
-        website = "https://morphe.software"
+        name = "Patcheddit"
+        description = "Morphe Patches for Reddit"
+        source = "git@github.com:wchill/patcheddit.git"
+        author = "wchill"
+        contact = "w@chill.dev"
+        website = "https://github.com/wchill/patcheddit"
         license = "Additional conditions under GPL section 7 apply: attribution and project name restrictions. See LICENSE file."
     }
 }
