32e92a07ed33696ee2c92c51eb2f916420803f48
Eric Ahn <wchill1337@gmail.com>
2026-01-19 05:19:44 -0800

    fix: Remove Infinity for Reddit patches (use Continuum for Reddit instead)

--- patches/src/main/kotlin/app/morphe/patches/reddit/customclients/infinityforreddit/api/Fingerprints.kt
+++
@@ -1,7 +0,0 @@
-package app.morphe.patches.reddit.customclients.infinityforreddit.api
-
-import app.morphe.patcher.Fingerprint
-
-internal val apiUtilsFingerprint = Fingerprint(
-    strings = listOf("native-lib")
-)
