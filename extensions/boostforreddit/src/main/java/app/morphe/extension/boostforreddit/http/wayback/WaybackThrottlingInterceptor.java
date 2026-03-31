3d0e5364f93f693bc1d9db7ca3069c5d2e488f2c
Eric Ahn <wchill1337@gmail.com>
2026-03-31 01:56:24 -0500

    chore: Clarify NOTICE

--- extensions/boostforreddit/src/main/java/app/morphe/extension/boostforreddit/http/wayback/WaybackThrottlingInterceptor.java
+++ extensions/boostforreddit/src/main/java/app/morphe/extension/boostforreddit/http/wayback/WaybackThrottlingInterceptor.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2026 wchill.
+ * https://github.com/wchill/patcheddit
+ *
+ * See the included NOTICE file for GPLv3 §7(b) and §7(c) terms that apply to this code.
+ */
+
 package app.morphe.extension.boostforreddit.http.wayback;
 
 import org.jetbrains.annotations.NotNull;
--- extensions/boostforreddit/src/main/java/app/morphe/extension/boostforreddit/utils/CacheUtils.java
+++ extensions/boostforreddit/src/main/java/app/morphe/extension/boostforreddit/utils/CacheUtils.java
@@ -1,11 +1,13 @@
+/*
+ * Copyright 2026 wchill.
+ * https://github.com/wchill/patcheddit
+ *
+ * See the included NOTICE file for GPLv3 §7(b) and §7(c) terms that apply to this code.
+ */
+
 package app.morphe.extension.boostforreddit.utils;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 
 import app.morphe.extension.shared.Utils;
 
