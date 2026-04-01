ed2697d3ab6eb796c505627b5da1d36b4b09b563
Eric Ahn <wchill1337@gmail.com>
2026-04-01 11:55:57 -0700

    fix: Remove dependency on transformInstructions patch

--- patches/src/main/kotlin/app/morphe/patches/reddit/customclients/relay/fix/slink/Fingerprints.kt
+++ patches/src/main/kotlin/app/morphe/patches/reddit/customclients/relay/fix/slink/Fingerprints.kt
@@ -2,6 +2,7 @@ package app.morphe.patches.reddit.customclients.relay.fix.slink
 
 import app.morphe.patcher.Fingerprint
 import app.morphe.patcher.OpcodeFilter
+import app.morphe.patcher.fieldAccess
 import app.morphe.patcher.methodCall
 import app.morphe.patcher.string
 import com.android.tools.smali.dexlib2.Opcode
@@ -17,4 +18,19 @@ internal val intentFilterActivityFingerprint = Fingerprint(
         OpcodeFilter(Opcode.MOVE_RESULT_OBJECT)
     ),
     custom = { _, classDef -> classDef.type == "Lreddit/news/IntentFilterActivity;" }
+)
+
+internal val refreshTokenAccessFilter = fieldAccess(
+    definingClass = "Lreddit/news/oauth/reddit/model/RedditAccessToken;",
+    name = "refreshToken",
+    opcode = Opcode.IPUT_OBJECT,
+)
+
+internal val refreshTokenAccessFingerprint = Fingerprint(
+    filters = listOf(
+        refreshTokenAccessFilter
+    ),
+    custom = { _, classDef ->
+        !classDef.type.startsWith("Lapp/morphe/")
+    }
 )
--- patches/src/main/kotlin/app/morphe/patches/reddit/customclients/relay/fix/slink/FixSLinksPatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/reddit/customclients/relay/fix/slink/FixSLinksPatch.kt
@@ -4,17 +4,14 @@ import app.morphe.patcher.extensions.InstructionExtensions.addInstruction
 import app.morphe.patcher.extensions.InstructionExtensions.addInstructionsWithLabels
 import app.morphe.patcher.extensions.InstructionExtensions.getInstruction
 import app.morphe.patcher.util.smali.ExternalLabel
-import app.morphe.patches.all.misc.transformation.transformInstructionsPatch
 import app.morphe.patches.reddit.customclients.RESOLVE_S_LINK_METHOD
 import app.morphe.patches.reddit.customclients.SET_ACCESS_TOKEN_METHOD
 import app.morphe.patches.reddit.customclients.fixSLinksPatch
 import app.morphe.patches.reddit.customclients.relay.RelayCompatible
 import app.morphe.patches.reddit.customclients.relay.misc.extension.sharedExtensionPatch
-import app.morphe.util.getReference
-import com.android.tools.smali.dexlib2.Opcode
+import app.morphe.util.findInstructionIndicesReversedOrThrow
 import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.instruction.TwoRegisterInstruction
-import com.android.tools.smali.dexlib2.iface.reference.FieldReference
 
 const val EXTENSION_CLASS_DESCRIPTOR = "Lapp/morphe/extension/relay/FixSLinksPatch;"
 
@@ -22,42 +19,23 @@ const val EXTENSION_CLASS_DESCRIPTOR = "Lapp/morphe/extension/relay/FixSLinksPat
 val fixSLinksPatch = fixSLinksPatch(
     extensionPatch = sharedExtensionPatch,
 ) {
-    dependsOn(
-        transformInstructionsPatch(
-            filterMap = { classDef, method, instruction, index ->
-                // Find all instructions that set a Reddit refresh token.
-                if (classDef.type.startsWith("Lapp/morphe")) {
-                    return@transformInstructionsPatch null
-                }
+    compatibleWith(*RelayCompatible)
 
-                val ref = instruction.getReference<FieldReference>()
-                if (instruction.opcode != Opcode.IPUT_OBJECT || ref == null) {
-                    return@transformInstructionsPatch null
-                }
-
-                if (ref.definingClass != "Lreddit/news/oauth/reddit/model/RedditAccessToken;" || ref.name != "refreshToken") {
-                    return@transformInstructionsPatch null
-                }
-
-                return@transformInstructionsPatch instruction to index
-            },
-            transform = { method, entry ->
+    execute {
+        refreshTokenAccessFingerprint.matchAll().forEach { match ->
+            match.method.findInstructionIndicesReversedOrThrow(refreshTokenAccessFilter).forEach { index ->
                 // Now inject a call to our extension method to also set the access token there.
-                val (instruction, index) = entry
+                val instruction = match.method.getInstruction(index)
                 val register = (instruction as TwoRegisterInstruction).registerA
-                method.addInstruction(
+                match.method.addInstruction(
                     index,
                     """
                         invoke-static { v$register }, $EXTENSION_CLASS_DESCRIPTOR->$SET_ACCESS_TOKEN_METHOD
                     """
                 )
             }
-        )
-    )
+        }
 
-    compatibleWith(*RelayCompatible)
-
-    execute {
         // region Patch navigation handler.
 
         intentFilterActivityFingerprint.method.apply {
